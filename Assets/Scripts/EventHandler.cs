ad030258341d3e59176ae89b244305645b9c2c62
Tyler <tyler@programmer.net>
2020-02-21 23:33:33 -0500

    Basic event handler.

---
+++ Assets/Scripts/EventHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventHandler : MonoBehaviour
+{
+    // Grabbing the object clicked at
+    private GameObject hitObject;
+
+    // Grabbing the EventObject script from the object clicked at
+    private EventObject eventObject;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                // Assign any objects that want to be triggered with the Trigger tag
+                if (hit.transform.tag == "Trigger")
+                {
+
+                    hitObject = hit.transform.gameObject;
+                    CanTriggerEvent();
+
+                }
+            }
+        }
+    }
+
+    // This will see if we've already triggered this object, and if we can trigger it again (if it's reusable)
+    void CanTriggerEvent()
+    {
+        eventObject = hitObject.GetComponent<EventObject>();
+        
+        if (eventObject.isReusable == true)
+        {
+            Debug.Log("Running event!");
+        }
+        else
+        {
+            if (eventObject.isUsable == true)
+            {
+                Debug.Log("Running event!");
+                eventObject.isUsable = false;
+            }
+            else
+            {
+                Debug.Log("You cannot run this event again.");
+            }
+        }
+    }
+
+    void TriggerEvent()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/EventObject.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventObject : MonoBehaviour
+{
+
+    public bool isReusable;
+    public bool isUsable;
+
+}

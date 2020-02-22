18e4845a638bbb6cae716e927f89289e7891011b
Scott <wolfofodingaming@gmail.com>
2020-02-22 04:49:58 +0200

    Basic Nav Mesh With Agent
    This is a basic nav mesh with the agent. Right now the agent just follows the "Ghost". I did that for basic testing purposes. Later we can add some of the basic triggers like tapping, throwing/ect

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;   
+
+public class Agent : MonoBehaviour
+{
+    public GameObject npcTarget;
+    public NavMeshAgent navMeshAgent;   
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.SetDestination(npcTarget.transform.position);
+    }
+}

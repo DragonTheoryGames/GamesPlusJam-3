2eb419357ff01dc03a06999cfdfd8cc46b539841
Scott <wolfofodingaming@gmail.com>
2020-02-23 04:53:41 +0200

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,26 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DoorMove : MonoBehaviour
+public class Door : MonoBehaviour
 {
     public GameObject door;
+    public float yRotation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     void OnCollisionEnter(Collision doorMove)
     {
-        if(doorMove.gameObject.tag == "Ghost/Agent")
+        if(doorMove.gameObject.name == "Agent")
         {
-
+            door.transform.Rotate(0, yRotation, 0);
         }
     }
+
+    void OnCollisionExit(Collision doorClose)
+    {
+        door.transform.Rotate(0, yRotation, 0);
+    }
+
 }

f20fd07f12daf548133d15561fe7513fc613e779
Cody Fulfer <Codeman1010@gmail.com>
2020-02-23 08:40:39 -0700

    Added keys and exit

---
+++ Assets/Scripts/Keys.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Keys : MonoBehaviour
+{
+    int keysLeft = 8;
+
+    private void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "key")
+        {
+            keysLeft -= 1;
+            Destroy(col.gameObject);
+        }
+        else if (col.gameObject.tag == "exit")
+        {
+            if (keysLeft <= 0)
+            {
+                Debug.Log("Win");
+            }
+        }
+    }
+
+
+}

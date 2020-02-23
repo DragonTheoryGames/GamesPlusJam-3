430a79b3fec67b602b36dfc47db439eebb87d551
Tyler <tyler@programmer.net>
2020-02-23 12:51:29 -0500

    Finished main menu and credits.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void playGame()
+    {
+        SceneManager.LoadScene("test_Mansion");
+    }
+
+    public void loadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void toMenu()
+    {
+        SceneManager.LoadScene("Main Menu");
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+}

aceef9ffb558fb8cc3f47e4ddc198a883833bf8f
Cody Fulfer <Codeman1010@gmail.com>
2020-02-23 11:17:05 -0700

    EnemyLight Prefab

---
+++ Assets/Scripts/EnemyLight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLight : MonoBehaviour
+{
+    bool hittingwall = false;
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col);
+        if (col.gameObject.tag == "wall")
+        {
+            hittingwall = true;
+        }
+        if (col.gameObject.tag == "agent" && !hittingwall)
+        {
+            GameOver();
+        }
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "wall")
+        {
+            hittingwall = false;
+        }
+    }
+
+    void GameOver(){
+        Debug.Log("gameover");
+    }
+
+
+
+
+}

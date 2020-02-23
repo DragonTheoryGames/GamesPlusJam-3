ea0ce9725cc5b0065a3be6867e589274a4e66990
Cody Fulfer <Codeman1010@gmail.com>
2020-02-23 06:48:18 -0700

    edited sound

---
+++ Assets/Scripts/GhostAudio.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostAudio : MonoBehaviour
+{
+    public AudioSource evpSource;
+
+    private void Start() 
+    {
+        evpSource = GetComponent<AudioSource>();
+    }
+
+    private void Update() {
+        if (Input.GetMouseButton(1))
+        {
+            evpSource.Play();
+        }
+    }
+}
